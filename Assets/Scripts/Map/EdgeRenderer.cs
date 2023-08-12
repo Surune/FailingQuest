43f934597b7740563f847e9e41f302cfae02337f
muzigae <mujigae1020@gmail.com>
2023-08-13 02:23:15 +0900

    Map Scripts by muzigae

---
+++ Assets/Scripts/Map/EdgeRenderer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Map
+{
+    public class EdgeRenderer : MonoBehaviour
+    {
+        private LineRenderer lineRenderer;
+        private Renderer renderer;
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

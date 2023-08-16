7ec5111c92a8d75e04c10c13bd6e11a79e7aebbd
mirlee0304 <danzzydala@gmail.com>
2023-08-16 21:45:19 +0900

    획득보물 작업중

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Treasure : MonoBehaviour
+{
+    private GameObject[] treasures;
+    public GameObject[] myTreasure;
+    public int prefabIndex;
+    
+
+    private Vector2 treasurePosition = new Vector2(-616, 174);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInParent<Button>().gameObject.SetActive(true);
+        treasures = FindObjectOfType<TreasureList>().treasurePrefab;
+
+        if (myTreasure.Length == 0)
+        {
+            GameManager.treasureCount = 0;
+            myTreasure = new GameObject[16];
+        }
+        else
+        {
+            GameManager.treasureCount = myTreasure.Length;
+        }
+        prefabIndex = Random.Range(0, 4);
+        myTreasure[GameManager.treasureCount] = Instantiate(treasures[prefabIndex], new Vector2(0, 0), transform.rotation, GameObject.Find("Canvas").transform);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowTreasure()
+    {
+        myTreasure[GameManager.treasureCount].transform.localPosition = treasurePosition;
+        myTreasure[GameManager.treasureCount].transform.localScale = new Vector2(100, 100);
+        treasurePosition.x += 2;
+        GameManager.treasureCount++;
+        GetComponentInParent<Button>().gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TreasureList.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureList : MonoBehaviour
+{
+    public GameObject[] treasurePrefab;
+    string currentIndex;
+    public Text EffectInfo;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        if(currentIndex == "gold")
+        {
+            //골드 획득
+            //GameManager.money += ?
+        }
+       
+    }
+
+    private void OnMouseEnter()
+    {
+        currentIndex = this.gameObject.name;
+        Debug.Log(currentIndex);
+        if (currentIndex == "gold(Clone)")//골드
+        {
+            EffectInfo.text = "골드 획득";
+        }
+        else if (currentIndex == "red_book(Clone)")//빨간책 
+        {
+            EffectInfo.text = "스킬 강화";
+        }
+        else if (currentIndex == "green_book(Clone)")//초록책 
+        {
+            EffectInfo.text = "스킬 강화";
+        }
+        else if (currentIndex == "blue_book(Clone)")//파란책 
+        {
+            EffectInfo.text = "스킬 강화";
+        }
+        EffectInfo.transform.localPosition= new Vector2(transform.localPosition.x+60, transform.localPosition.y-60);
+        EffectInfo.gameObject.SetActive(true);
+
+    }
+
+    
+    private void OnMouseExit()
+    {
+        EffectInfo.gameObject.SetActive(false);
+    }
+    
+
+
+}

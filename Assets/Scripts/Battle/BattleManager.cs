9e6008ce08fb727f4d958db5b00032ae07247a4c
0114kek <0114kek@snu.ac.kr>
2023-08-19 20:35:54 +0900

    feat: 타겟팅 추가중

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour //전투의 진행을 담당
 {
     private int time;
-    public List<GameObject> location;
-    public Character _target_test;
+    public List<LocationHelper> location;
+    private Character target = null; // 스킬 타겟
+    private Vector3 target_position = new Vector3(0, 0, 0); // 이동 스킬 타겟 위치
 
     void Start()
     {
@@ -30,13 +32,31 @@ public class BattleManager : MonoBehaviour //전투의 진행을 담당
      */
     public Character getTarget()
     {
-        return _target_test;
-        //throw new NotImplementedException();
+        return target;
+    }
+
+    public void setTarget(Character character) //이벤트 방식으로 전환 필요
+    {
+        target = character;
+    }
+
+    public void resetTarget()
+    {
+        target = null;
     }
 
     public Vector3 getPosition()
     {
         return location[0].transform.position;
-        //throw new NotImplementedException();
+    }
+
+    public void setTargetPosition(Vector3 position)
+    {
+        target_position = position;
+    }
+
+    public void resetTargetPosition()
+    {
+        target_position = new Vector3(0, 0, 0);
     }
 }
--- Assets/Scripts/Battle/Character.cs
+++ Assets/Scripts/Battle/Character.cs
@@ -19,9 +19,11 @@ public class Character : MonoBehaviour
 
     // 보유한 스킬 리스트
     public List<Skill> Skills = new List<Skill>();
+    public BattleManager battleManager = null;
 
     private void Start()
     {
+        Debug.Log(gameObject.name);
         if (Skills.Count > 8)
         {
             throw new Exception("Skill Count Exceeds 8");
@@ -30,6 +32,11 @@ public class Character : MonoBehaviour
         if (position == -1)
         {
             throw new Exception("Position not initialized");
+        }
+
+        if (battleManager == null)
+        {
+            throw new Exception("Character: Battlemanager not initialized");
         }
 
         _initialHP = HP;
@@ -60,5 +67,10 @@ public class Character : MonoBehaviour
     public void updateHPBar()
     {
         HPStatus.value = (float)HP / _initialHP;
+    }
+
+    public void OnMouseUp()
+    {
+        battleManager.setTarget(this);
     }
 }

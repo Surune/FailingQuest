d42755a65884873d59230fbccc1b4c0eaaf46932
muzigae <mujigae1020@gmail.com>
2023-08-24 00:27:28 +0900

    EdgeRenderer: Useless code deleted

--- Assets/Scripts/Map/EdgeRenderer.cs
+++ Assets/Scripts/Map/EdgeRenderer.cs
@@ -17,16 +17,6 @@ namespace Map
         {
             lineRenderer = GetComponent<LineRenderer>();
             renderer = GetComponent<Renderer>();
-            renderer.material.mainTextureScale =
-                new Vector2(Vector2.Distance(lineRenderer.GetPosition(0), lineRenderer.GetPosition(lineRenderer.positionCount - 1)) / lineRenderer.widthMultiplier,
-                    1);
-        }
-
-        private void Update()
-        {
-            renderer.material.mainTextureScale =
-                new Vector2(Vector2.Distance(lineRenderer.GetPosition(0), lineRenderer.GetPosition(lineRenderer.positionCount - 1)) / lineRenderer.widthMultiplier,
-                    1);
         }
     }
 }
